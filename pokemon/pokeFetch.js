fa3df8642870984eabfd77407b76abbbc2c1af5d
unknown <80522547+Arrshe@users.noreply.github.com>
2021-11-15 18:37:08 +0100

    cambiado pokeFetch.js y creado iniciarJuego

--- pokemon/pokeFetch.js
+++ pokemon/pokeFetch.js
@@ -1,28 +1,33 @@
-/*Creates random number for pokemon selection*/
-let randNum = Math.floor((Math.random() * 901))
-console.log(randNum)
-let pokeName, pokeId, sprite, isLegendary;
+import fetch from "node-fetch";
+
+function traerPokemon() {
+    /*Creates random number for pokemon selection*/
+    let randNum = Math.floor((Math.random() * 901))
+    console.log(randNum)
+    let pokeName, pokeId, sprite, isLegendary;
+
+    // console.log(`https://pokeapi.co/api/v2/pokemon/${randNum}`)
+
+    /*Fetch the specific pokemon with the number generated */
+    let find = fetch(`https://pokeapi.co/api/v2/pokemon/${randNum}`).then(response =>
+        response.json()
+    ).then(data => {
+        pokeName = data.name
+        pokeId = data.id
+        sprite = data.sprites.other["official-artwork"].front_default
+        console.log(data, data.types)
+    })
+    let findLegendary = fetch(`https://pokeapi.co/api/v2/pokemon-species/${randNum}`).then(response => response.json()).then(data => isLegendary = data.is_legendary)
 
 
-// console.log(`https://pokeapi.co/api/v2/pokemon/${randNum}`)
-
-/*Fetch the specific pokemon with the number generated */
-let find = fetch(`https://pokeapi.co/api/v2/pokemon/${randNum}`).then(response =>
-  response.json()
-).then(data => {
-  pokeName = data.name
-  pokeId = data.id
-  sprite = data.sprites.other["official-artwork"].front_default
-  console.log(data, data.types)
-})
-let findLegendary = fetch(`https://pokeapi.co/api/v2/pokemon-species/${randNum}`).then(response => response.json()).then(data => isLegendary = data.is_legendary)
-
-
-const datosInicio = function () {
-  setTimeout(() => {
-    console.log(pokeName, pokeId, sprite, isLegendary)
-  }, 1000)
+    const datosInicio = function () {
+        setTimeout(() => {
+            console.log(pokeName, pokeId, sprite, isLegendary)
+        }, 1000)
+    }
+    datosInicio()
 }
-datosInicio()
 
-export {pokeName, pokeId, sprite, isLegendary};
+export { traerPokemon };
+
+console.log(traerPokemon());
