d79eb96b396819351a234d328be6de9f6e2e359a
Joseba Ibarlucea <Jibarlucea91@gmail.com>
2021-11-15 17:23:18 +0100

    Primer ejemplo para pedir datos especificos sobre el pokemon a utilizar

---
+++ pokemon/pokeFetch.js
@@ -0,0 +1,26 @@
+/*Creates random number for pokemon selection*/
+let randNum = Math.floor((Math.random() * 901))
+console.log(randNum)
+let pokeName, pokeId, sprite, isLegendary;
+
+
+// console.log(`https://pokeapi.co/api/v2/pokemon/${randNum}`)
+
+/*Fetch the specific pokemon with the number generated */
+let find = fetch(`https://pokeapi.co/api/v2/pokemon/${randNum}`).then(response =>
+  response.json()
+).then(data => {
+  pokeName = data.name
+  pokeId = data.id
+  sprite = data.sprites.other["official-artwork"].front_default
+  console.log(data, data.types)
+})
+let findLegendary = fetch(`https://pokeapi.co/api/v2/pokemon-species/${randNum}`).then(response => response.json()).then(data => isLegendary = data.is_legendary)
+
+
+const datosInicio = function () {
+  setTimeout(() => {
+    console.log(pokeName, pokeId, sprite, isLegendary)
+  }, 1000)
+}
+datosInicio()
