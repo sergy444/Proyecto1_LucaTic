710861d35c2aa70867202c92ee4cf4d5da76fa9b
Joseba Ibarlucea <Jibarlucea91@gmail.com>
2021-11-16 12:03:19 +0100

    Cambios de usuario para guardar en localStorage

--- pokemon/pokeApp.js
+++ pokemon/pokeApp.js
@@ -1,29 +1,38 @@
 import Usuario from "./usuario.js";
-import {pokeName} from "./pokeFetch.js";
+import {
+  pokeName
+} from "./pokeFetch.js";
 
 let intentosRestantes = 3;
 let numRespuestaCorrecta = 0;
 
 function crearUsuario() {
-    let nombreUsuario = document.getElementById("nombre").value;
-    let usuario = new Usuario(nombreUsuario, 0);
-    console.log(usuario);
+  let nombreUsuario = document.getElementById("nombre").value;
+  let usuario = new Usuario(nombreUsuario, 0);
+  localStorage.setItem("usuario", JSON.stringify(usuario))
+  console.log(usuario);
 }
 
 function validarRespuesta() {
 
-    //guardamos el valor que el usuario escribe en el imput
-    let valorNombre = document.querySelector("#nombre-pokemon").value;
+  //guardamos el valor que el usuario escribe en el imput
+  let valorNombre = document.querySelector("#nombre-pokemon").value;
+  const sumarScore = function () {
+    return numRespuestaCorrecta++
+  }
+  const contarIntentos = function () {
+    return intentosRestantes--
+  }
 
-    //comparamos el valor que pone el usuario con la variable que guarda el nombre del pokemon actual
-    if (valorNombre == pokeName) {
-        window.alert("respuesta correcta");
-        numRespuestaCorrecta++;
-    } else {
-        window.alert("respuesta incorrecta");
-        intentosRestantes--;
-    }
-    console.log(numRespuestaCorrecta, intentosRestantes);
+  //comparamos el valor que pone el usuario con la variable que guarda el nombre del pokemon actual
+  if (valorNombre == pokeName) {
+    window.alert("respuesta correcta");
+    sumarScore()
+  } else {
+    window.alert("respuesta incorrecta");
+    contarIntentos()
+  }
+  console.log(numRespuestaCorrecta, intentosRestantes);
 }
 
 let newuser = document.getElementById("boton-comenzar");
