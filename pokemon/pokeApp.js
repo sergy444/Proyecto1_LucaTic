81ab30d0857046ff4fcb9cf4d6c1ca1a23d99287
unknown <80522547+Arrshe@users.noreply.github.com>
2021-11-16 15:18:27 +0100

    resulto problema pokeApp.js

--- pokemon/pokeApp.js
+++ pokemon/pokeApp.js
@@ -19,34 +19,34 @@ let genPoke;
 
 function crearUsuario() {
 
-  let nombreUsuario = document.getElementById("nombre").value;
-  let usuario = new Usuario(nombreUsuario, 0);
-  //Guarda el usuario creado en el localStorage para pedirlo luego 
-  //-Joseba
-  localStorage.setItem("usuario", JSON.stringify(usuario))
+    let nombreUsuario = document.getElementById("nombre").value;
+    let usuario = new Usuario(nombreUsuario, 0);
+    //Guarda el usuario creado en el localStorage para pedirlo luego 
+    //-Joseba
+    localStorage.setItem("usuario", JSON.stringify(usuario))
 }
 const mostrarScore = function () {
-  let usuario1 = JSON.parse(localStorage.getItem("usuario"))
-  //Copia de los scores predeterminados
-  //-Joseba
-  let highScores = JSON.parse(localStorage.getItem("scores"))
-  //Evalua el valor de intentos para sumar el total de puntaje 
-  //-Joseba
-  if (intentosRestantes === 0) {
-    usuario1.puntuacion = Number(numRespuestaCorrecta) + 50;
-    //Desabilita el boton para dejar de intentar
+    let usuario1 = JSON.parse(localStorage.getItem("usuario"))
+    //Copia de los scores predeterminados
     //-Joseba
-    validar.disabled = true
-    localStorage.setItem("scores", JSON.stringify([usuario1, ...highScores]))
-  }
-  localStorage.setItem("usuario", JSON.stringify(usuario1))
+    let highScores = JSON.parse(localStorage.getItem("scores"))
+    //Evalua el valor de intentos para sumar el total de puntaje 
+    //-Joseba
+    if (intentosRestantes === 0) {
+        usuario1.puntuacion = Number(numRespuestaCorrecta) + 50;
+        //Desabilita el boton para dejar de intentar
+        //-Joseba
+        validar.disabled = true
+        localStorage.setItem("scores", JSON.stringify([usuario1, ...highScores]))
+    }
+    localStorage.setItem("usuario", JSON.stringify(usuario1))
 
 
-  console.log(usuario1)
+    console.log(usuario1)
 
 }
 
-function validarRespuesta() {
+
 
 
 //funcion para saber de que generacion es el pokemon
@@ -55,41 +55,43 @@ function queGeneracion() {
         genPoke = "El pokemon es de primera generación.";
     } else if (pokeId >= 152 && pokeId <= 251) {
         genPoke = "El pokemon es de segunda generación.";
-    }else if (pokeId >= 252 && pokeId <= 386) {
+    } else if (pokeId >= 252 && pokeId <= 386) {
         genPoke = "El pokemon es de tercera generación.";
-    }else if (pokeId >= 387 && pokeId <= 493) {
+    } else if (pokeId >= 387 && pokeId <= 493) {
         genPoke = "El pokemon es de cuarta generación.";
-    }else if (pokeId >= 494 && pokeId <= 649) {
+    } else if (pokeId >= 494 && pokeId <= 649) {
         genPoke = "El pokemon es de quinta generación.";
-    }else if (pokeId >= 650 && pokeId <= 721) {
+    } else if (pokeId >= 650 && pokeId <= 721) {
         genPoke = "El pokemon es de sexta generación.";
-    }else if (pokeId >= 722 && pokeId <= 809) {
+    } else if (pokeId >= 722 && pokeId <= 809) {
         genPoke = "El pokemon es de séptima generación.";
-    }else if (pokeId >= 810 && pokeId <= 898) {
+    } else if (pokeId >= 810 && pokeId <= 898) {
         genPoke = "El pokemon es de octava generación.";
     }
     return genPoke;
+}
 
-  //guardamos el valor que el usuario escribe en el imput
-  let valorNombre = document.querySelector("#nombre-pokemon").value;
-  const sumarScore = function () {
-    return numRespuestaCorrecta++
-  }
-  const contarIntentos = function () {
-    const negative = intentosRestantes <= 0 ? intentosRestantes = 0 : intentosRestantes--;
-    return negative
-  }
+function validarRespuesta() {
+    //guardamos el valor que el usuario escribe en el imput
+    let valorNombre = document.querySelector("#nombre-pokemon").value;
+    const sumarScore = function () {
+        return numRespuestaCorrecta++
+    }
+    const contarIntentos = function () {
+        const negative = intentosRestantes <= 0 ? intentosRestantes = 0 : intentosRestantes--;
+        return negative
+    }
 
-  //comparamos el valor que pone el usuario con la variable que guarda el nombre del pokemon actual
-  if (valorNombre == pokeName) {
-    window.alert("respuesta correcta");
-    sumarScore()
-  } else {
-    window.alert("respuesta incorrecta");
-    contarIntentos()
-  }
-  console.log(numRespuestaCorrecta, intentosRestantes);
-  mostrarScore()
+    //comparamos el valor que pone el usuario con la variable que guarda el nombre del pokemon actual
+    if (valorNombre == pokeName) {
+        window.alert("respuesta correcta");
+        sumarScore()
+    } else {
+        window.alert("respuesta incorrecta");
+        contarIntentos()
+    }
+    console.log(numRespuestaCorrecta, intentosRestantes);
+    mostrarScore()
 }
 //funcion para que segun un num aleatorio imprima una pista en pantalla
 function mostrarPista() {
@@ -141,4 +143,3 @@ pista.addEventListener("click", mostrarPista);
 export {
     validarRespuesta
 };
-
