ad0bee93c5c9caad781f28174801f025c740b3f8
unknown <80522547+Arrshe@users.noreply.github.com>
2021-11-16 09:18:22 +0100

    solucionado error de cargarJuego

--- pokemon/pokeFetch.js
+++ pokemon/pokeFetch.js
@@ -1,4 +1,11 @@
-import fetch from "node-fetch";
+    //escondo el contenedor de la primera página
+    const seleccion = document.getElementById("seleccion");
+
+    //muestro el contenedor de la segunda página
+    const juego = document.getElementById("juego");
+
+    //guardamos en una variable el tag de la imagen vacío
+    let imgPokemon = document.getElementById("imagen-pokemon");
 
 function traerPokemon() {
     /*Creates random number for pokemon selection*/
@@ -24,11 +31,23 @@ let findLegendary = fetch(`https://pokeapi.co/api/v2/pokemon-species/${randNum}`
 const datosInicio = function () {
   setTimeout(() => {
     console.log(pokeName, pokeId, sprite, isLegendary, types)
+    
+    //rellenamos la ruta de la imagen con la url del sprite
+    imgPokemon.src = `${sprite}`;
   }, 1000)
     }
     datosInicio()
 }
 
-export { traerPokemon };
+function cargarJuego(){
 
-console.log(traerPokemon());
+    seleccion.classList.add("hidden");
+    juego.classList.remove("hidden");
+
+    traerPokemon();
+
+}
+
+//añadimos al botón de comenzar la función para cargar el juego
+let cargar = document.getElementById("boton-comenzar");
+cargar.addEventListener("click",cargarJuego);
