e57e9c1f80b648785ea43b930844aaad80f4d770
Joseba Ibarlucea <Jibarlucea91@gmail.com>
2021-11-16 10:14:42 +0100

    Creado los high scores y añadidos al localStorage para ser utilizados

--- pokemon/usuario.js
+++ pokemon/usuario.js
@@ -5,4 +5,21 @@ class Usuario {
     }
 }
 
+const highScores = [{
+        username: "Red",
+        score: 9999
+    }, {
+        username: "Ash",
+        score: 2500
+    },
+    {
+        username: "Misty",
+        score: 2000
+    },
+]
+
+localStorage.setItem("scores", JSON.stringify(highScores))
+
+
+
 export default Usuario;
